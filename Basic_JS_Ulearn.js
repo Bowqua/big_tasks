3aa9d2b863eb60876d4cb90f88e615256a0a0253
Quatov <128380652+Bowqua@users.noreply.github.com>
2026-02-18 18:32:40 +0500

    Added files from Ulearn Web

---
+++ Basic_JS_Ulearn.js
@@ -0,0 +1,56 @@
+﻿/*
+//2
+function convertStringToNumber (str) {
+    return Number.isInteger(str) ? parseInt(str) : false;
+}
+*/
+
+//4
+/*
+function createGratitude (name, rating = 0) {
+    if (!name)
+        name = 'Аноним';
+
+    return `${name} оценил нас на ${rating} из 5. Спасибо, ${name}!`;
+}
+*/
+
+/*
+//5
+function checkA1 (a) {
+    // 1. if-else
+    if (a !== 0)
+        return a;
+    else
+        return 'Все плохо';
+}
+
+function checkA2 (a) {
+    // 2. тернарный оператор
+    return a !== 0 ? a : 'Все плохо';
+}
+
+function checkA3 (a) {
+    // 3. логическое или
+    return a || 'Все плохо';
+}
+*/
+
+/*
+//6
+function sumSquares (min, max) {
+    let result = 0;
+    for (let i = min; i <= max; i++) {
+        result += i * i;
+    }
+
+    return result;
+}
+*/
+
+/*
+//7
+function sumArray (arr) {
+    return arr.flat(Infinity).reduce((a, b) => a + b, 0);
+}
+*/
---
+++ Structure_data_Ulearn.js
@@ -0,0 +1,97 @@
+﻿/*
+//1
+function divide (a, b) {
+    return Math.trunc(a / b);
+}
+*/
+
+/*
+//2
+function truncateString (str, len) {
+    return str.length > len ? str.substring(0, len) + '…' : str;
+}
+*/
+
+/*
+//3
+function palindromeChecker (str) {
+    return str.toLowerCase().split('').reverse().join('') === str ? true : false;
+}
+*/
+
+/*
+//4
+function formatMoney (money) {
+    let fixed = Number(money).toFixed(2);
+    let parts = fixed.split('.');
+
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+
+    return parts.join('.');
+}
+*/
+
+/*
+//5
+function getMostFrequentElement (arr) {
+    let elements = {};
+    let result = null;
+    let maxCount = 0;
+
+    for (let number of arr) {
+        elements[number] = (elements[number] || 0) + 1;
+
+        if (elements[number] > maxCount) {
+            maxCount = elements[number];
+            result = number;
+        }
+    }
+
+    return result;
+}
+*/
+
+/*
+//6
+function sample (arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+*/
+
+/*
+//7
+function square (arr) {
+    return arr.map(a => a ** 2);
+}
+*/
+
+/*
+//8
+function sortArr(arr) {
+    return [...arr].sort((a, b) => b - a);
+}
+*/
+
+/*
+//9
+function calculateAverage() {
+    let sum = 0;
+
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+
+    return arguments.length === 0 ? 0 : sum / arguments.length;
+}
+*/
+
+/*
+//10
+function formatDate(date) {
+    let day = String(date.getDate()).padStart(2, '0');
+    let month = String(date.getMonth() + 1).padStart(2, '0');
+    let year = String(date.getFullYear());
+
+    return `${day}-${month}-${year}`;
+}
+*/
